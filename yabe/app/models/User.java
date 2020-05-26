beb8393bd1375095038b7b0c7061f0a6b0d4f743
Alex Malo <amalo@uiowa.edu>
2020-05-26 15:13:11 -0500

    added User class and User test

---
+++ yabe/app/models/User.java
@@ -0,0 +1,29 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.jpa.*;
+
+//@Entity annotation marks class as JPA entity, Model superclass provides JPA helpers
+//ie Model superclass probides auto @Id which is required by JPA
+@Entity
+public class User extends Model {
+
+    //Play automatically encapsulates variables and provides getters and setters
+    //otherwise leaving everything public would be a no no
+    public String email;
+    public String password;
+    public String fullname;
+    public boolean isAdmin;
+
+    public User(String email, String password, String fullname) {
+        this.email = email;
+        this.password = password;
+        this.fullname = fullname;
+    }
+
+    public static User connect(String email, String password) {
+        return find("byEmailAndPassword", email, password).first();
+    }
+}
--- yabe/test/BasicTest.java
+++ yabe/test/BasicTest.java
@@ -6,8 +6,13 @@ import models.*;
 public class BasicTest extends UnitTest {
 
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void tryConnectAsUser() {
+        new User("bob@yahoo.com", "secret", "Bob").save();
+
+        //Test
+        assertNotNull(User.connect("bob@yahoo.com", "secret"));
+        assertNull(User.connect("bob@yahoo.com", "badpassword"));
+        assertNull(User.connect("tom@yahoo.com", "secret"));
     }
 
 }
