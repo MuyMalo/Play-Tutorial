1c6ceb17abc822a86f7f505adbdfc66fd0f67a45
Alex Malo <amalo@uiowa.edu>
2020-05-27 09:16:23 -0500

    added post tagging

--- yabe/app/models/Post.java
+++ yabe/app/models/Post.java
@@ -22,8 +22,14 @@ public class Post extends Model {
     @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
     public List<Comment> comments;
 
+    @ManyToMany(cascade=CascadeType.PERSIST)
+    public Set<Tag> tags;
+
     public Post(User author, String title, String content) {
         this.comments = new ArrayList<Comment>();
+        //use a TreeSet here in order to keep the tag list in a predictable order
+        //(alphabetical order in fact, because of our previous compareTo implementation)
+        this.tags = new TreeSet<Tag>();
         this.author = author;
         this.title = title;
         this.content = content;
@@ -35,6 +41,17 @@ public class Post extends Model {
         this.comments.add(newComment);
         this.save();
         return this;
+    }
+    
+    public Post tagItWith(String name) {
+        tags.add(Tag.findOrCreateByName(name));
+        return this;
+    }
+
+    public static List<Post> findTaggedWith(String tag) {
+        return Post.find(
+                "select distinct p from Post p join p.tags as t where t.name = ?", tag
+        ).fetch();
     }
 
     //got an error about using just ? instead of ?1
