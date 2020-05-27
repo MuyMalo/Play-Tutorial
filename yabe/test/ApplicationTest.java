00f5091bc5597d1b485fdf52500c9b2b2f408fbf
Alex Malo <amalo@uiowa.edu>
2020-05-27 15:07:54 -0500

    prepared for production

--- yabe/test/ApplicationTest.java
+++ yabe/test/ApplicationTest.java
@@ -4,6 +4,7 @@ import play.mvc.*;
 import play.mvc.Http.*;
 import models.*;
 
+//FunctionalTest supplies useful utility helpers
 public class ApplicationTest extends FunctionalTest {
 
     @Test
@@ -11,7 +12,14 @@ public class ApplicationTest extends FunctionalTest {
         Response response = GET("/");
         assertIsOk(response);
         assertContentType("text/html", response);
-        assertCharset(play.Play.defaultWebEncoding, response);
+        assertCharset("utf-8", response);
     }
-    
+
+    @Test
+    public void testAdminSecurity() {
+        Response response = GET("/admin");
+        assertStatus(302, response);
+        assertHeaderEquals("Location", "/login", response);
+    }
+
 }
